58ed1ec60a72a47f91da503660373e786ef1f6bd
Andrew Grishin <kirivanov777@gmail.com>
2022-12-04 01:15:05 +0300

    change bot work from webhook to longpoll

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-import { webhookCallback } from 'grammy';
 import app from './server';
 import { bot } from './init';
 import { startup, updateWidgets } from './bot';
-import { BOT_TOKEN, PORT } from './config';
+import { PORT } from './config';
 
 // start bot
 startup();
 
 setInterval(updateWidgets, 1000 * 60);
 
+// start bot
+bot.start();
+
 // start express server
-app.use(`/${BOT_TOKEN}`, webhookCallback(bot, 'express'));
 app.listen(Number(PORT), async () => {
   console.log(
     `The web server succesfully starts up and listens on port ${PORT}!`
